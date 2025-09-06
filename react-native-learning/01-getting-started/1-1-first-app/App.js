872fc373a10dce68b2d5a6f10cfd825b842585f6
Shivam-Raj <srvcode@gmail.com>
2025-09-06 15:42:51 +0530

    Adding 01-getting started apps

---
+++ react-native-learning/01-getting-started/1-1-first-app/App.js
@@ -0,0 +1,28 @@
+/**
+ *   - Step 1: Run the app via npm expo start
+ *   - Step 2: Press a
+ *   - Step 3: If app doesn't reload, press r
+ *   - Step 4: If app still doesn't reload, close the expo app on android emulator or phone & open again
+ *   - Step 5: Press a, it will load the content to the phone or emulator
+ */
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the 1st App created by Shivam Raj</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
