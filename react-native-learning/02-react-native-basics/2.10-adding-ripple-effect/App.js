21140be286af6659fe014778838f4f9e922d57eb
Shivam RaJ <geek.shivamraj@gmail.com>
2025-11-08 15:24:09 +0530

    Add impl for ripple effect

---
+++ react-native-learning/02-react-native-basics/2.10-adding-ripple-effect/App.js
@@ -0,0 +1,47 @@
+import {FlatList, StyleSheet, View} from 'react-native';
+import {useState} from "react";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+export default function App() {
+
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    const addGoalHandler = (enteredGoalText) => {
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, {
+            text: enteredGoalText, id: Math.random().toString()
+        }]);
+    };
+
+    const deleteGoalHandler = (id) => {
+        setCourseGoals(currentCourseGoals => {
+            return currentCourseGoals.filter(goal => goal.id !== id);
+        });
+    }
+
+    return (<View style={styles.appContainer}>
+        <GoalInput onAddGoal={addGoalHandler}/>
+        <View style={styles.goalsContainer}>
+            <FlatList data={courseGoals}
+                      renderItem={(itemData) => {
+                          return (
+                              <GoalItem
+                                  text={itemData.item.text}
+                                  id={itemData.item.id}
+                                  onDeleteItem={deleteGoalHandler}
+                              />
+                          )
+                      }}
+                      keyExtractor={(item, index) => item.id}/>
+        </View>
+    </View>);
+}
+
+const styles = StyleSheet.create({
+    appContainer: {
+        flex: 1, paddingTop: 50, paddingHorizontal: 16
+    },
+    goalsContainer: {
+        flex: 5,
+    }
+});
