59c54a694e6cbebcf5d1c3c7224d8f38ce2d2a09
Shivam RaJ <geek.shivamraj@gmail.com>
2025-11-14 19:26:52 +0530

    Adding impl for meal details screen.

---
+++ react-native-learning/05-react-native-navigation/5.8-adding-meal-details-screen/App.js
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+import CategoriesScreen from "./screens/CategoriesScreen";
+import {StatusBar} from "expo-status-bar";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
+
+const Stack = createNativeStackNavigator();
+
+/**
+ *  Adding & Configuring the Meal Details Screen
+ *
+ */
+export default function App() {
+    return (
+        <>
+            <StatusBar style='light'/>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="MealsCategories" screenOptions={{
+                    headerStyle: {backgroundColor: '#351401'},
+                    headerTintColor: 'white',
+                    contentStyle: {backgroundColor: '#3f2f25'}
+                }}>
+                    <Stack.Screen name='MealsCategories' component={CategoriesScreen} options={{ title: 'All Categories' }} />
+                    <Stack.Screen name='MealsOverview' component={MealsOverviewScreen}/>
+                    <Stack.Screen name='MealDetail' component={MealDetailScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+});
