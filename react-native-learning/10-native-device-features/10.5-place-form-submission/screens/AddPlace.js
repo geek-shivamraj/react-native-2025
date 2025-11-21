7967080f94b68471a86f8e986d11269bd38218b0
Shivam RaJ <geek.shivamraj@gmail.com>
2025-11-21 10:05:33 +0530

    Adding impl for device persistence

---
+++ react-native-learning/10-native-device-features/10.5-place-form-submission/screens/AddPlace.js
@@ -0,0 +1,16 @@
+import PlaceForm from "../components/Places/PlaceForm";
+
+const AddPlace = ({navigation}) => {
+
+    const createPlaceHandler = (place) => {
+        navigation.navigate('AllPlaces', {
+            place: place
+        });
+    };
+
+    return (
+        <PlaceForm onCreatePlace={createPlaceHandler}/>
+    );
+}
+
+export default AddPlace;
---
+++ react-native-learning/10-native-device-features/10.6-persisting-places/screens/AllPlaces.js
@@ -0,0 +1,28 @@
+import PlacesList from "../components/Places/PlacesList";
+import {useEffect, useState} from "react";
+import {useIsFocused} from "@react-navigation/native";
+import {fetchPlaces} from "../util/database";
+
+const AllPlaces = () => {
+
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+
+        const loadPlaces = async () => {
+            const places = await fetchPlaces();
+            setLoadedPlaces(places);
+        }
+
+        if(isFocused){
+            loadPlaces();
+        }
+    }, [isFocused]);
+
+    return (
+        <PlacesList places={loadedPlaces} />
+    );
+}
+
+export default AllPlaces;
