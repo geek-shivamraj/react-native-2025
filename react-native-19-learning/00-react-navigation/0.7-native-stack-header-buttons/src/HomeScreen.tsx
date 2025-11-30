a5265b3342e06bde8bcdbf9e598a70823396aa70
Shivam RaJ <geek.shivamraj@gmail.com>
2025-11-30 16:54:56 +0530

    Adding native stack header button impl

---
+++ react-native-19-learning/00-react-navigation/0.7-native-stack-header-buttons/src/HomeScreen.tsx
@@ -0,0 +1,29 @@
+import {Text, View} from "react-native";
+import {Button} from "@react-navigation/elements";
+import {useNavigation} from "@react-navigation/native";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {AppParamList} from "./App";
+import {useEffect, useState} from "react";
+
+type HomeScreenNavProp = NativeStackNavigationProp<AppParamList>;
+
+export default function HomeScreen() {
+
+    const navigation = useNavigation<HomeScreenNavProp>();
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // Use `setOptions` to update the button that we previously specified
+        // Now the button includes an `onPress` handler to update the count
+        navigation.setOptions({
+            headerRight: () => (
+                <Button onPress={() => setCount((c) => c + 1)}>Update count</Button>
+            ),
+        });
+    }, [navigation]);
+
+    return (
+        <Text>Count: {count}</Text>
+    );
+}
